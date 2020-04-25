56f74402141961b30a5ceb551f0235218e647017
Moaath Alattas <MoaathAlattas@users.noreply.github.com>
2020-04-25 16:04:08 -0700

    add env var to nginx

--- client/web/next.config.js
+++ client/web/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        API_URL: `${process.env.PROTOCOL}${process.env.API_URL}${process.env.APP_PORT !== 80 ? `:${process.env.APP_PORT}` : null}`,
-        APP_URL: `${process.env.PROTOCOL}${process.env.HOST_URL}${process.env.APP_PORT !== 80 ? `:${process.env.APP_PORT}` : null}`,
+        API_URL: `${process.env.PROTOCOL}${process.env.API_URL}${process.env.APP_PORT !== '80' ? `:${process.env.APP_PORT}` : ''}`,
+        APP_URL: `${process.env.PROTOCOL}${process.env.HOST_URL}${process.env.APP_PORT !== '80' ? `:${process.env.APP_PORT}` : ''}`,
     },
 }
